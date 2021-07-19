af6dd1037a61ea60ee6520c6d15fb4883de3e7bd
Allysha Jovich <ajovich@hotmail.com>
2021-07-18 23:18:16 -0700

    cleaned up and deleted comments from login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,6 @@
-/* Selecting both id's togglePassword and password */
 const togglePassword = document.querySelector('#togglePassword');
 const password = document.querySelector('#password');
 
-/* Attaching an event listener to the togglePassword icon */
-/* Toggle type of password field & icon class */
 togglePassword.addEventListener('click', function (eyeball)  {
     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
     password.setAttribute('type', type);
@@ -13,12 +10,10 @@ togglePassword.addEventListener('click', function (eyeball)  {
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
-    // Collect values from the login form
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password').value.trim();
   
     if (email && password) {
-      // Send a POST request to the API endpoint
       const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
@@ -27,7 +22,6 @@ const loginFormHandler = async (event) => {
   
       if (response.ok) {
         console.log(response);
-        // If successful, redirect the browser to the profile page
         document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
